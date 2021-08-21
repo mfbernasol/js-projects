c7a45f90e8076ec2a00558d3d16fe30531e7d483
mfbernasol <mfbernasol@gmail.com>
2021-08-21 12:10:31 -0500

    create simple validation

--- script.js
+++ script.js
@@ -4,6 +4,58 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// show input error message
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// show success outline 
+function showSucces(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+
+}
+
+// Check email is valid 
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+//event listensers
 form.addEventListener('submit', function(e){
-    
+    e.preventDefault();
+
+    if(username.value === ''){
+        showError(username, 'Username is required');
+    }else{
+        showSucces(username);
+    }
+
+    if(email.value === ''){
+        showError(email, 'email is required');
+
+    }else if (!isValidEmail(email.value)){
+        showError(email,'Email is not valid');
+    }
+    else{
+        showSucces(email);
+    }
+
+    if(password.value === ''){
+        showError(password, 'password is required');
+    }else{
+        showSucces(password);
+        
+    }
+
+    if(password2.value === ''){
+        showError(password2, 'password is required');
+    }else{
+        showSucces(password2);
+        
+    }
 })
